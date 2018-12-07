0e862425363e8afb46ab35f4d4c347d5c1f257a0
Dmitry Chuveev <dmitry.chuveev@gmail.com>
2018-12-06 23:08:47 -0800

    Update sign_in_user.py

--- sign_in_user.py
+++ sign_in_user.py
@@ -1,3 +1,16 @@
+curl -H "Content-Type: application/json" -d "{\"email\":\"alina.korolevich@yopmail.com\",\"password\":\"internship\"}" http://local.school.portnov.com:4520/api/v1/sign-in
+or
+curl -X POST -H "Content-Type: application/json" -d "{\"email\":\"alina.korolevich@yopmail.com\",\"password\":\"internship\"}" http://local.school.portnov.com:4520/api/v1/sign-in
+"""
+{"user":{"id":949,"email":"alina.korolevich@yopmail.com",
+"name":"Alina Korolevich","group":"A01","role":"TEACHER",
+"createdAt":"2018-10-29T18:47:56.000Z",
+"updatedAt":"2018-10-29T18:52:22.000Z"},
+"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFsaW5hLmtvcm9sZXZpY2hAeW9wbWFpbC5jb20iLCJpYXQiOjE1NDMxOTM5MzIsImV4cCI6MTU0MzI4MDMzMn0.m1NwLoyQ9PV1Pw-ndXa83h_h1CP2rQ8vn1PT44e6M5o"}
+"""
+
+
+
 import requests
 import json
 
@@ -25,3 +38,4 @@ token = parsed_json["token"]
 "updatedAt":"2018-10-30T18:03:54.000Z"},
 "token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG4uc21pdGgzQHlvcG1haWwuY29tIiwiaWF0IjoxNTQwOTI0NTk4LCJleHAiOjE1NDEwMTA5OTh9.BYOWgao7TX1i3PAAEI1rogggUVsJH6PEmy6mCI8BZig"}
 """
+
